b9dd2120c555ca1f40a692e684db45ca3fabdccc
Rodrigo L. da Silveira <rodrigo.leao.silveira@outlook.com>
2021-07-11 23:45:30 -0300

    Layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import JogoPage from './pages/game/game-page';
 import LoginPage from './pages/start/start-page';
-import FoodComponent from './components/food/food-component';
 
 function App() {
 
@@ -12,41 +10,30 @@ function App() {
 
     <Router>
 
-      <FoodComponent></FoodComponent>
-
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-
+          Hackthon Herval - Sneake Game
         </header>
+
+        <div className="App-content">
+
+          <Switch>
+
+            <Route path="/jogo">
+              <JogoPage></JogoPage>
+            </Route>
+
+            <Route path="/">
+              <LoginPage></LoginPage>
+            </Route>
+
+          </Switch>
+
+        </div>
+
       </div>
 
-      <Switch>
-
-        <Route path="/jogo">
-          <JogoPage></JogoPage>
-        </Route>
-
-        <Route path="/">
-          <LoginPage></LoginPage>
-        </Route>
-
-      </Switch>
     </Router>
-
-
-
 
   );
 }
