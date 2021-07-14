d4653863800ee7ef26efe544256f6b76999210c1
Mateus Vier <mateusvier1993@gmail.com>
2021-07-14 00:34:57 -0300

    Alterado a página inicial

--- src/pages/start/start-page.tsx
+++ src/pages/start/start-page.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import "./start-page.css";
 
 const LoginPage = () => {
     return (
-        <div>
-            Login Page
-        </div>
+        
+        <>
+            <div className='corpo'>1) Mateus - 100 Pontos</div>
+
+            <form action="/form/submit" method="post">
+                <div>
+                    <label className='label'>Nome</label>
+                    <input type="text" />
+                </div>
+            </form>
+
+            <button className='button' type="button">Jogar!</button>
+      </>
+        
     );
 }
 
