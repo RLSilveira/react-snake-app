58d06ca6a3fcb389a356a0ce4c332982d140cae5
Mateus Vier <mateusvier1993@gmail.com>
2021-07-16 01:11:11 -0300

    Realizado alterações na página inicial

--- src/pages/start/start-page.tsx
+++ src/pages/start/start-page.tsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./start-page.css";
+import "../../services/records-service";
+import recordesService from "../../services/records-service";
+import { Record } from "../../models/record";
 
 const LoginPage = () => {
-    return (
-        
-        <>
-            <div className='corpo'>1) Mateus - 100 Pontos</div>
+  const [records, setRecords] = useState<Record[]>();
+  useEffect(() => {
+    getRecords();
+  }, []);
+  const getRecords = async () => {
+    var r = await recordesService.Get();
+    setRecords(r);
+  };
 
-            <form action="/form/submit" method="post">
-                <div>
-                    <label className='label'>Nome</label>
-                    <input type="text" />
-                </div>
-            </form>
+  return (
+    <>
+      <div className="corpo">
+        <h2>Recordes:</h2>
+        <ol>
+          {records?.map((record: Record, i: any) => {
+            return (
+              <li key={i}>
+                {record.user} {record.score}
+              </li>
+            );
+          })}
+        </ol>
+      </div>
 
-            <button className='button' type="button">Jogar!</button>
-      </>
-        
-    );
-}
+      <form action="/form/submit" method="post">
+        <div>
+          <label className="label">Nome: </label>
+          <input type="text" />
+        </div>
+      </form>
+
+      <button className="button" type="button">
+        Jogar!
+      </button>
+    </>
+  );
+};
 
 export default LoginPage;
